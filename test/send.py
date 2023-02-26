e48d2c683455fd19ae3ace088ebf89f44d596d4e
Drelf2018 <drelf2018@outlook.com>
2023-02-26 19:24:42 +0800

    fix: 避免重复提交

--- test/send.py
+++ test/send.py
@@ -2,6 +2,7 @@ from weibo_poster import BiliGo, DanmakuPost, Poster, RoomInfo, WeiboRequest, lo
 
 poster = Poster(188888131, "e2ac9d62-bf44-4672-8cbb-2b420bc5cb0f", "http://localhost:5664")
 biligo = BiliGo("poster", "http://localhost:8080", 14703541)
+PostList = list()
 
 async def uidFilter(_: RoomInfo, danmaku: DanmakuPost):
     if danmaku.uid == "188888131":
@@ -13,11 +14,14 @@ async def uidFilter(_: RoomInfo, danmaku: DanmakuPost):
 async def recv(roomInfo: RoomInfo, danmaku: DanmakuPost):
     logger.info(danmaku)
 
-# @Poster.job(name="七海", start=2, args=["7198559139"])
+@Poster.job(name="七海", start=2, args=["7198559139"])
 async def weibo(uid: str):
+    await poster.online()
     post = None
     async for post in session.posts(uid):
-        await poster.update(post)
+        if post.mid not in PostList:
+            PostList.append(post.mid)
+            await poster.update(post)
     if post is not None:
         async for comment in session.comments(post):
             if comment.uid == uid:
